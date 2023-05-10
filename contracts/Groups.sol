af822a93e3ab75b5285fc6da9dd0d4ca84b6d5b2
Daemon <69844188+daemon-reconfig@users.noreply.github.com>
2023-05-11 01:01:35 +0530

    Update Groups.sol

--- contracts/Groups.sol
+++ contracts/Groups.sol
@@ -5,9 +5,9 @@ contract Groups {
     struct Group {
         uint id;
         string name;
-        uint owner;
-        mapping(uint => bool) admins;
-        mapping(uint => bool) members;
+        address owner; // changed from uint to address
+        mapping(address => bool) admins;
+        mapping(address => bool) members;
         mapping(address => uint) roles;
         bytes32 groupKey;
         bool inviteOnly;
@@ -20,12 +20,12 @@ contract Groups {
     uint public reputationThreshold = 100;
     
     function createGroup(string memory name, bool inviteOnly) public {
-        require(userReputation[(msg.sender)] >= reputationThreshold, "Not enough reputation points to create group.");
+        require(userReputation[msg.sender] >= reputationThreshold, "Not enough reputation points to create group."); // removed unnecessary parentheses
         
         Group storage newGroup = groups[groupIdCounter];
         newGroup.id = groupIdCounter;
         newGroup.name = name;
-        newGroup.owner = uint256(msg.sender);
+        newGroup.owner = msg.sender;
         newGroup.admins[msg.sender] = true;
         newGroup.roles[msg.sender] = 2; // Owner
         newGroup.groupKey = keccak256(abi.encodePacked(name, block.timestamp, msg.sender));
@@ -33,21 +33,28 @@ contract Groups {
         
         groupIdCounter++;
     }
-    
-    function addAdmin(uint groupId, uint userId) public {
-        require(groups[groupId].roles[msg.sender] >= 2, "Not authorized to add admin.");
-        require(groups[groupId].members[userId], "User is not a member of the group.");
-        
-        groups[groupId].admins[userId] = true;
-        groups[groupId].roles[userId] = 1; // Admin
+    function addUser(uint groupId, address userAddress) public { // changed userId to userAddress
+        require(groups[groupId].roles[msg.sender] >= 1, "Not authorized to add user.");
+        require(!groups[groupId].members[userAddress], "User is already a member of the group."); // changed userId to userAddress
+
+        groups[groupId].members[userAddress] = true; // changed userId to userAddress
+        groups[groupId].roles[userAddress] = 0; // Member
     }
     
-    function removeAdmin(uint groupId, uint userId) public {
-        require(groups[groupId].roles[msg.sender] >= 2, "Not authorized to remove admin.");
-        require(groups[groupId].admins[userId], "User is not an admin of the group.");
+    function addAdmin(uint groupId, address userAddress) public { // changed userId to userAddress
+        require(groups[groupId].roles[msg.sender] >= 2, "Not authorized to add admin.");
+        require(groups[groupId].members[userAddress], "User is not a member of the group."); // changed userId to userAddress
         
-        groups[groupId].admins[userId] = false;
-        groups[groupId].roles[userId] = 0; // Member
+        groups[groupId].admins[userAddress] = true; // changed userId to userAddress
+        groups[groupId].roles[userAddress] = 1; // Admin
+    }
+    
+    function removeAdmin(uint groupId, address userAddress) public { // changed userId to userAddress
+        require(groups[groupId].roles[msg.sender] >= 2, "Not authorized to remove admin.");
+        require(groups[groupId].admins[userAddress], "User is not an admin of the group."); // changed userId to userAddress
+        
+        groups[groupId].admins[userAddress] = false; // changed userId to userAddress
+        groups[groupId].roles[userAddress] = 0; // Member
     }
     
     function joinGroup(uint groupId) public {
@@ -64,19 +71,22 @@ contract Groups {
         groups[groupId].roles[msg.sender] = 4; // Not a member
     }
     
-    function kickUser(uint groupId, uint userId) public {
+    function kickUser(uint groupId, address userAddress) public { // changed userId to userAddress
+        
+
         require(groups[groupId].roles[msg.sender] >= 2, "Not authorized to kick user.");
-        require(groups[groupId].members[userId], "User is not a member of the group.");
-        userId=uint(userId);
-        groups[groupId].members[userId] = false;
-        groups[groupId].roles[userId] = 4; // Not a member
+        require(groups[groupId].members[userAddress], "User is not a member of the group.");
+        
+        groups[groupId].members[userAddress] = false;
+        groups[groupId].roles[userAddress] = 4; // Not a member
     }
     
-    function banUser(uint groupId, uint userId) public {
+    function banUser(uint groupId, address userAddress) public {
         require(groups[groupId].roles[msg.sender] >= 2, "Not authorized to ban user.");
-        require(groups[groupId].members[userId], "User is not a member of the group.");
-        
-        groups[groupId].members[userId] = false;
-        groups[groupId].roles[userId]=5;
+        require(groups[groupId].roles[userAddress] != 4, "User is already banned from the group.");
+    
+        groups[groupId].members[userAddress] = false;
+        groups[groupId].roles[userAddress] = 5; // Banned
     }
+
 }
